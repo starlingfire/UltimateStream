4284bcf9e62d055589951dfa6fd8cc80210a4850
starlingfire <26200565+starlingfire@users.noreply.github.com>
2018-04-04 14:14:04 -0400

    Added logo toggle
    Added:
     - logo toggle
     - sample stream background

--- src/script.js
+++ src/script.js
@@ -5,16 +5,20 @@ function updateValue(str){
 	var oldValue = document.getElementById(dest).innerHTML;
 	var newValue = str.value;
 	
+	// Show the alert for debugging
 	if (document.getElementById("debug").checked) {
 		alert("New value: " + newValue + "\nOld value: " + oldValue + "\nSource: " + src + "\nDest: " + dest);
 	}
-		
+	
+	// Neither of these cases fire if updating the team name
 	switch(newValue){
 		
+		// Increase the score by 1
 		case "inc":
 			newValue = parseInt(oldValue) + 1;
 			break;
-			
+		
+		// Decrease the score by 1		
 		case "dec":
 			newValue = parseInt(oldValue) - 1;
 			break;
@@ -22,4 +26,17 @@ function updateValue(str){
 	
 	// Update the field
 	document.getElementById(dest).innerHTML = newValue;
+}
+
+function toggleLogo(){
+	
+	// Show the alert for debugging
+	if (document.getElementById("debug").checked) 
+		alert(document.getElementById("logo").style.display);
+	
+	// Toggle the display style of the logo column
+	if(document.getElementById("logo").style.display == "none")
+		document.getElementById("logo").style.display = "";
+	else
+		document.getElementById("logo").style.display = "none";
 }
